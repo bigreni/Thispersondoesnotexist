c8db5dfaefb9f9274123bba348c7a514f9a8b293
bigreni <bigreni@gmail.com>
2019-02-16 17:14:04 -0600

    Version 1

---
+++ www/js/main.js
@@ -0,0 +1,95 @@
+    function onLoad() {
+        if ((/(ipad|iphone|ipod|android|windows phone)/i.test(navigator.userAgent))) {
+            document.addEventListener('deviceready', checkFirstUse, false);
+        } else {
+            notFirstUse();
+        }
+    }
+
+  var admobid = {};
+  if( /(android)/i.test(navigator.userAgent) ) { // for android & amazon-fireos
+    admobid = {
+      banner: 'ca-app-pub-1683858134373419/7790106682', // or DFP format "/6253334/dfp_example_ad"
+      interstitial: 'ca-app-pub-9249695405712287/5288129821'
+    };
+  } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) { // for ios
+    admobid = {
+      banner: 'ca-app-pub-1683858134373419/7790106682', // or DFP format "/6253334/dfp_example_ad"
+      interstitial: 'ca-app-pub-9249695405712287/8955912090'
+    };
+  }
+
+    function initApp() {
+        if (!AdMob) { alert('admob plugin not ready'); return; }
+        initAd();
+        //display interstitial at startup
+        loadInterstitial();
+    }
+    function initAd() {
+        var defaultOptions = {
+            position: AdMob.AD_POSITION.BOTTOM_CENTER,
+            bgColor: 'black', // color name, or '#RRGGBB'
+            isTesting: false // set to true, to receiving test ad for testing purpose
+        };
+        AdMob.setOptions(defaultOptions);
+        registerAdEvents();
+    }
+    // optional, in case respond to events or handle error
+    function registerAdEvents() {
+        // new events, with variable to differentiate: adNetwork, adType, adEvent
+        document.addEventListener('onAdFailLoad', function (data) {
+            document.getElementById('screen').style.display = 'none';     
+        });
+        document.addEventListener('onAdLoaded', function (data) { });
+        document.addEventListener('onAdPresent', function (data) { });
+        document.addEventListener('onAdLeaveApp', function (data) { });
+        document.addEventListener('onAdDismiss', function (data) { 
+            document.getElementById('screen').style.display = 'none';     
+        });
+    }
+
+    function createSelectedBanner() {
+          AdMob.createBanner({adId:admobid.banner});
+    }
+
+    function loadInterstitial() {
+        AdMob.prepareInterstitial({ adId: admobid.interstitial, isTesting: false, autoShow: true });
+    }
+
+   function checkFirstUse()
+    {
+        window.ga.startTrackerWithId('UA-88579601-9', 1, function(msg) {
+            window.ga.trackView('Home');
+        });
+        initApp();
+        askRating();
+        //document.getElementById('screen').style.display = 'none';     
+    }
+
+   function notFirstUse()
+    {
+        document.getElementById('screen').style.display = 'none';     
+    }
+
+function askRating()
+{
+  AppRate.preferences = {
+  openStoreInApp: true,
+  useLanguage:  'en',
+  usesUntilPrompt: 10,
+  promptAgainForEachNewVersion: true,
+  storeAppURL: {
+                ios: '1296111737',
+                android: 'market://details?id=com.persondoesnotexist.free'
+               }
+};
+ 
+AppRate.promptForRating(false);
+}
+
+function reload()
+{
+    d = new Date();
+    var img = document.getElementById("imgPerson");
+    img.src = "https://www.thispersondoesnotexist.com/?" + d.getTime();
+}
