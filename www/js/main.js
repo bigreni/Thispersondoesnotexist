a90d3cb7eecc01d94ebe4f2a0ae079148ad63c98
bigreni <bigreni@gmail.com>
2019-02-18 11:28:15 -0600

    Version 2

--- www/js/main.js
+++ www/js/main.js
@@ -103,3 +103,30 @@ function reload()
     var img = document.getElementById("imgPerson");
     img.src = "https://www.thispersondoesnotexist.com/?" + d.getTime();
 }
+
+function fbShare(){
+	window.plugins.socialsharing.shareViaFacebook('Message via Facebook', 
+                                                 null /* img */, 
+                                                 null /* url */, 
+                                                 null, 
+                                                 function(errormsg){alert("Error: Cannot Share")}
+                                                 );
+};
+ 
+function whatsappShare(){
+	window.plugins.socialsharing.shareViaWhatsApp('Message via WhatsApp',
+                                                 null /* img */,
+                                                 "https://pointdeveloper.com/" /* url */, 
+                                                 null, 
+                                                 function(errormsg){alert("Error: Cannot Share")}
+                                                 );
+};
+ 
+function twitterShare(){
+	 window.plugins.socialsharing.shareViaTwitter('Message via Twitter',
+                                                 null /* img */, 
+                                                 'http://twitter.com/', 
+                                                 null, 
+                                                 function(errormsg){alert("Error: Cannot Share")}
+                                                 );
+  };
